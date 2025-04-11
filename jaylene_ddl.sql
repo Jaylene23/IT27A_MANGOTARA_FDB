724dafcd782fc2e9c0cfbca851a5e799a928f413
Jaylene23 <itsmejaylene23@gmail.com>
2025-04-11 15:44:43 +0800

    Update jaylene_ddl.sql

--- jaylene_ddl.sql
+++ jaylene_ddl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2025 at 06:13 AM
+-- Generation Time: Apr 10, 2025 at 05:30 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `group4ddl`
+-- Database: `jayleneddl`
 --
 
 -- --------------------------------------------------------
